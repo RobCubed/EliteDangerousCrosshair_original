141a4da82251b7786f0b348087981101c3953826
RobCubed <rwhit85@gmail.com>
2014-11-14 21:28:39 -0500

    Detects game status, hides crosshair if game is inactive

--- EDCrossHair/Form1.cs
+++ EDCrossHair/Form1.cs
@@ -12,8 +12,6 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 
-// TODO: Detect if game closes or is closed, wait for it to open!
-// TODO: Hide if Elite Dangerous is NOT the active window!!!
 // TODO: Hotkey to hide the crosshair
 // TODO: Cursor styles!
 
@@ -21,64 +19,6 @@ namespace EDCrossHair
 {
     public partial class Form1 : Form
     {
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        public static extern IntPtr FindWindow(string strClassName, string strWindowName);
-
-        [DllImport("user32.dll")]
-        public static extern bool GetWindowRect(IntPtr hwnd, ref Rect rectangle);
-
-        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
-        static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
-
-        [DllImport("user32.dll")]
-        static extern IntPtr GetMenu(IntPtr hWnd);
-
-        [DllImport("user32.dll")]
-        static extern int GetMenuItemCount(IntPtr hMenu);
-
-        [DllImport("user32.dll")]
-        static extern bool DrawMenuBar(IntPtr hWnd);
-
-        [DllImport("user32.dll")]
-        static extern bool RemoveMenu(IntPtr hMenu, uint uPosition, uint uFlags);
-
-        [DllImport("USER32.DLL")]
-        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
-
-        [DllImport("USER32.DLL")]
-        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-
-        [DllImport("user32.dll")]
-        static extern bool GetClientRect(IntPtr hWnd, out RECT lpRect);
-
-        [DllImport("user32.dll")]
-        static extern bool ClientToScreen(IntPtr hWnd, ref Point lpPoint);
-
-        [DllImport("user32.dll")]
-        static extern bool SetCursorPos(int X, int Y);
-
-        public static uint MF_BYPOSITION = 0x400;
-        public static uint MF_REMOVE = 0x1000;
-        public static int GWL_STYLE = -16;
-        public static int WS_BORDER = 0x00800000; //window with border
-        public static int WS_DLGFRAME = 0x00400000; //window with double border but no title
-        public static int WS_CAPTION = WS_BORDER | WS_DLGFRAME; //window with a title bar
-
-
-        public struct Rect {
-           public int Left { get; set; }
-           public int Top { get; set; }
-           public int Right { get; set; }
-           public int Bottom { get; set; }
-        }
-
-        public static RECT GetClientRect(IntPtr hWnd)
-        {
-            RECT result = new RECT();
-            GetClientRect(hWnd, out result);
-            return result;
-        }
-
         public Form1()
         {
             
@@ -90,22 +30,35 @@ namespace EDCrossHair
 
         private void RunPaint()
         {
-            Process[] processes = Process.GetProcessesByName("EliteDangerous32");
-            Process lol = processes[0];
-            IntPtr ptr = lol.MainWindowHandle;
-            Rect NotepadRect = new Rect();
-            GetWindowRect(ptr, ref NotepadRect);
-            RECT clientRect = GetClientRect(ptr);
-
-            Console.WriteLine(clientRect.Height + " " + clientRect.Width);
+            Console.WriteLine("Welcome to EliteDangerousCrosshair by RobCubed!");
+            Console.WriteLine("http://www.github.com/RobCubed");
+            Console.WriteLine("Remember, Elite Dangerous *must* be in windowed mode for this to function.");
+            Console.WriteLine("--------------------------------------------");
+            string gameName = "EliteDangerous32";
+            IntPtr ptr = ProcessHandler.WaitForProcessByName(gameName);
+            EDCrossHair.ProcessHandler.RECT clientRect = ProcessHandler.GetRect(ptr);
+            
             int x;
             int y;
             bool coordinatesFound;
             int newPointX;
             int newPointY;
+            
 
             while (true)
             {
+                ptr = ProcessHandler.WaitForProcessByName(gameName);
+                clientRect = ProcessHandler.GetRect(ptr);
+
+                if (ProcessHandler.IsForeground(ptr))
+                {
+                    bufferedPanel.programFocus = true;
+                }
+                else
+                {
+                    bufferedPanel.programFocus = false;
+                }
+
                 Point point;
                 x = clientRect.Width / 2;
                 y = clientRect.Height / 2;
@@ -113,18 +66,18 @@ namespace EDCrossHair
 
                 point = new Point();
 
-                coordinatesFound = ClientToScreen(ptr, ref point);
+                coordinatesFound = ProcessHandler.ClientToScreen(ptr, ref point);
 
                 newPointX = point.X + x;
                 newPointY = point.Y + y;
 
+
+
                 if (coordinatesFound == true && (newPointX != bufferedPanel.xDraw || newPointY != bufferedPanel.yDraw))
                 {
-                    Console.WriteLine("Moved: " + (point.X + x) + " " + (point.Y + y));
                     bufferedPanel.xDraw = newPointX;
                     bufferedPanel.yDraw = newPointY;
                 }
-
             }
         }
 
@@ -138,100 +91,5 @@ namespace EDCrossHair
             }
         }
 
-        [StructLayout(LayoutKind.Sequential)]
-        public struct RECT
-        {
-            public int Left, Top, Right, Bottom;
-
-            public RECT(int left, int top, int right, int bottom)
-            {
-                Left = left;
-                Top = top;
-                Right = right;
-                Bottom = bottom;
-            }
-
-            public RECT(System.Drawing.Rectangle r) : this(r.Left, r.Top, r.Right, r.Bottom) { }
-
-            public int X
-            {
-                get { return Left; }
-                set { Right -= (Left - value); Left = value; }
-            }
-
-            public int Y
-            {
-                get { return Top; }
-                set { Bottom -= (Top - value); Top = value; }
-            }
-
-            public int Height
-            {
-                get { return Bottom - Top; }
-                set { Bottom = value + Top; }
-            }
-
-            public int Width
-            {
-                get { return Right - Left; }
-                set { Right = value + Left; }
-            }
-
-            public System.Drawing.Point Location
-            {
-                get { return new System.Drawing.Point(Left, Top); }
-                set { X = value.X; Y = value.Y; }
-            }
-
-            public System.Drawing.Size Size
-            {
-                get { return new System.Drawing.Size(Width, Height); }
-                set { Width = value.Width; Height = value.Height; }
-            }
-
-            public static implicit operator System.Drawing.Rectangle(RECT r)
-            {
-                return new System.Drawing.Rectangle(r.Left, r.Top, r.Width, r.Height);
-            }
-
-            public static implicit operator RECT(System.Drawing.Rectangle r)
-            {
-                return new RECT(r);
-            }
-
-            public static bool operator ==(RECT r1, RECT r2)
-            {
-                return r1.Equals(r2);
-            }
-
-            public static bool operator !=(RECT r1, RECT r2)
-            {
-                return !r1.Equals(r2);
-            }
-
-            public bool Equals(RECT r)
-            {
-                return r.Left == Left && r.Top == Top && r.Right == Right && r.Bottom == Bottom;
-            }
-
-            public override bool Equals(object obj)
-            {
-                if (obj is RECT)
-                    return Equals((RECT)obj);
-                else if (obj is System.Drawing.Rectangle)
-                    return Equals(new RECT((System.Drawing.Rectangle)obj));
-                return false;
-            }
-
-            public override int GetHashCode()
-            {
-                return ((System.Drawing.Rectangle)this).GetHashCode();
-            }
-
-            public override string ToString()
-            {
-                return string.Format(System.Globalization.CultureInfo.CurrentCulture, "{{Left={0},Top={1},Right={2},Bottom={3}}}", Left, Top, Right, Bottom);
-            }
-        }
     }
 }
---
+++ EDCrossHair/ProcessHandler.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EDCrossHair
+{
+    class ProcessHandler
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr FindWindow(string strClassName, string strWindowName);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hwnd, ref Rect rectangle);
+
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetMenu(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        static extern int GetMenuItemCount(IntPtr hMenu);
+
+        [DllImport("user32.dll")]
+        static extern bool DrawMenuBar(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        static extern bool RemoveMenu(IntPtr hMenu, uint uPosition, uint uFlags);
+
+        [DllImport("USER32.DLL")]
+        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("USER32.DLL")]
+        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetClientRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll")]
+        public static extern bool ClientToScreen(IntPtr hWnd, ref Point lpPoint);
+
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowThreadProcessId(IntPtr hWnd, out uint ProcessId);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("kernel32.dll")]
+        static extern int GetProcessId(IntPtr handle);
+
+        public static uint MF_BYPOSITION = 0x400;
+        public static uint MF_REMOVE = 0x1000;
+        public static int GWL_STYLE = -16;
+        public static int WS_BORDER = 0x00800000; //window with border
+        public static int WS_DLGFRAME = 0x00400000; //window with double border but no title
+        public static int WS_CAPTION = WS_BORDER | WS_DLGFRAME; //window with a title bar
+
+        
+        public static IntPtr GetSingleProcessByName(String name) {
+            Process[] processes = Process.GetProcessesByName(name);
+            Process p = processes[0];
+            return p.MainWindowHandle;
+        }
+
+        internal static bool IsForeground(IntPtr ptr)
+        {
+            IntPtr hwnd = ProcessHandler.GetForegroundWindow();
+            if (hwnd.ToString().Equals(ptr.ToString()))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static RECT GetRect(IntPtr ptr)
+        {
+            return GetClientRect(ptr);
+        }
+
+        internal static bool IsRunning(string gameName)
+        {
+            Process[] pname = Process.GetProcessesByName(gameName);
+            if (pname.Length == 0)
+                return false;
+            else
+                return true;
+        }
+
+        internal static IntPtr WaitForProcessByName(string name)
+        {
+            bool hooked = false;
+            bool notice = false;
+            IntPtr ptr = (IntPtr)0;
+            while (!hooked)
+            {
+                try
+                {
+                     ptr = GetSingleProcessByName(name);
+                     hooked = true;
+                     break;
+                }
+                catch (Exception e)
+                {
+                    if (!notice)
+                    {
+                        Console.WriteLine("Waiting for Elite Dangerous to launch...");
+                        notice = true;
+                    }
+                }
+                Thread.Sleep(1000);
+            }
+            return ptr;
+        }
+
+        public struct Rect
+        {
+            public int Left { get; set; }
+            public int Top { get; set; }
+            public int Right { get; set; }
+            public int Bottom { get; set; }
+        }
+
+
+        public static RECT GetClientRect(IntPtr hWnd)
+        {
+            RECT result = new RECT();
+            GetClientRect(hWnd, out result);
+            return result;
+        }
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left, Top, Right, Bottom;
+
+            public RECT(int left, int top, int right, int bottom)
+            {
+                Left = left;
+                Top = top;
+                Right = right;
+                Bottom = bottom;
+            }
+
+            public RECT(System.Drawing.Rectangle r) : this(r.Left, r.Top, r.Right, r.Bottom) { }
+
+            public int X
+            {
+                get { return Left; }
+                set { Right -= (Left - value); Left = value; }
+            }
+
+            public int Y
+            {
+                get { return Top; }
+                set { Bottom -= (Top - value); Top = value; }
+            }
+
+            public int Height
+            {
+                get { return Bottom - Top; }
+                set { Bottom = value + Top; }
+            }
+
+            public int Width
+            {
+                get { return Right - Left; }
+                set { Right = value + Left; }
+            }
+
+            public System.Drawing.Point Location
+            {
+                get { return new System.Drawing.Point(Left, Top); }
+                set { X = value.X; Y = value.Y; }
+            }
+
+            public System.Drawing.Size Size
+            {
+                get { return new System.Drawing.Size(Width, Height); }
+                set { Width = value.Width; Height = value.Height; }
+            }
+
+            public static implicit operator System.Drawing.Rectangle(RECT r)
+            {
+                return new System.Drawing.Rectangle(r.Left, r.Top, r.Width, r.Height);
+            }
+
+            public static implicit operator RECT(System.Drawing.Rectangle r)
+            {
+                return new RECT(r);
+            }
+
+            public static bool operator ==(RECT r1, RECT r2)
+            {
+                return r1.Equals(r2);
+            }
+
+            public static bool operator !=(RECT r1, RECT r2)
+            {
+                return !r1.Equals(r2);
+            }
+
+            public bool Equals(RECT r)
+            {
+                return r.Left == Left && r.Top == Top && r.Right == Right && r.Bottom == Bottom;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (obj is RECT)
+                    return Equals((RECT)obj);
+                else if (obj is System.Drawing.Rectangle)
+                    return Equals(new RECT((System.Drawing.Rectangle)obj));
+                return false;
+            }
+
+            public override int GetHashCode()
+            {
+                return ((System.Drawing.Rectangle)this).GetHashCode();
+            }
+
+            public override string ToString()
+            {
+                return string.Format(System.Globalization.CultureInfo.CurrentCulture, "{{Left={0},Top={1},Right={2},Bottom={3}}}", Left, Top, Right, Bottom);
+            }
+        }
+
+
+    }
+}
